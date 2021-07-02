bb642fc971d31c8278d7f0ced0b34a8e52e3cd8f
Shivangi Singh <singhshivangi1909@gmail.com>
2021-07-02 21:53:36 +0530

    initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyASsPG6H3AlNNMf2hUR-TsEe9GiDJeZJUk",
+    authDomain: "oshop-6e0e2.firebaseapp.com",
+    projectId: "oshop-6e0e2",
+    storageBucket: "oshop-6e0e2.appspot.com",
+    messagingSenderId: "820656463993",
+    appId: "1:820656463993:web:ddcf1a4b8be84dde5af4a8",
+    measurementId: "G-8R3V7TJ1ZY"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyASsPG6H3AlNNMf2hUR-TsEe9GiDJeZJUk",
+    authDomain: "oshop-6e0e2.firebaseapp.com",
+    projectId: "oshop-6e0e2",
+    storageBucket: "oshop-6e0e2.appspot.com",
+    messagingSenderId: "820656463993",
+    appId: "1:820656463993:web:ddcf1a4b8be84dde5af4a8",
+    measurementId: "G-8R3V7TJ1ZY"
+  }
 };
 
 /*
